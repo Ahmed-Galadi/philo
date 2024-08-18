7aa99f095f84b746eb2d21696b31091989009380
Ahmed-Galadi <galadiahmed99@gmail.com>
2024-08-19 00:10:03 +0100

    ...

--- print.c
+++ print.c
@@ -1,13 +1,17 @@
 #include "philo.h"
 
-void	print_state(t_philo *philo, t_state state, bool )
+void	print_state(t_philo *philo, t_state state, bool debug)
 {
 	long	time_passed;
 
 	time_passed = get_time(MILISEC) - philo->data->start_simulation;
-	if ()
 	handle_mutex(&philo->data->mutex_print, LOCK);
-	if ((state == FORK1_TAKE || state == FORK2_TAKE) && !is_sim_end(philo->data))
-		printf(WHITE"%-6ld"RESET" %d has taken a fork\n", time_passed, philo->philo_id);
+	if (debug)
+		print_state_debug(state, philo, time_passed);
+	else
+	{
+		if ((state == FORK1_TAKE || state == FORK2_TAKE) && !is_sim_end(philo->data))
+			printf(WHITE"%-6ld"RESET" %d has taken a fork\n", time_passed, philo->philo_id);
+	}
 	handle_mutex(&philo->data->mutex_print, UNLOCK);
 }
