a2568edc0f6646dd0855570a2331583951e51a9b
Ahmed-Galadi <galadiahmed99@gmail.com>
2024-08-19 16:53:01 +0100

    i did made a debugging function for print_state

--- print.c
+++ print.c
@@ -1,10 +1,28 @@
 #include "philo.h"
 
+void	print_state_debug(t_philo *philo, t_state state, long time_passed)
+{
+	if (state == FORK1_TAKE&& !is_sim_end(philo->data))
+		printf(WHITE"%6ld"RESET" %d has taken first fork [%d]\n", time_passed, philo->philo_id, philo->fork_1->fork_id);
+	else if (state == FORK2_TAKE&& !is_sim_end(philo->data))
+		printf(WHITE"%6ld"RESET" %d has taken second fork [%d]\n", time_passed, philo->philo_id, philo->fork_2->fork_id);
+	else if (state == EATING && !is_sim_end(philo->data))	
+		printf(WHITE"%6ld"RESET" %d is eating\tmeals[%d]\n", time_passed, philo->philo_id, philo->meals_count);
+	else if (state == THINKING && !is_sim_end(philo->data))	
+		printf(WHITE"%6ld"RESET" %d is thinking\n", time_passed, philo->philo_id);
+	else if (state == SLEEPING && !is_sim_end(philo->data))	
+		printf(WHITE"%6ld"RESET" %d is sleeping\n", time_passed, philo->philo_id);
+	else if (state == DEATH)	
+		printf(RED"%6ld"RESET" %d is dead\n", time_passed, philo->philo_id);
+}
+
 void	print_state(t_philo *philo, t_state state, bool debug)
 {
 	long	time_passed;
 
 	time_passed = get_time(MILISEC) - philo->data->start_simulation;
+	if (philo->is_full)
+		return ;
 	handle_mutex(&philo->data->mutex_print, LOCK);
 	if (debug)
 		print_state_debug(state, philo, time_passed);
@@ -12,6 +30,14 @@ void	print_state(t_philo *philo, t_state state, bool debug)
 	{
 		if ((state == FORK1_TAKE || state == FORK2_TAKE) && !is_sim_end(philo->data))
 			printf(WHITE"%-6ld"RESET" %d has taken a fork\n", time_passed, philo->philo_id);
+		else if (state == EATING && !is_sim_end(philo->data))	
+			printf(WHITE"%-6ld"RESET" %d is eating\n", time_passed, philo->philo_id);
+		else if (state == THINKING && !is_sim_end(philo->data))	
+			printf(WHITE"%-6ld"RESET" %d is thinking\n", time_passed, philo->philo_id);
+		else if (state == SLEEPING && !is_sim_end(philo->data))	
+			printf(WHITE"%-6ld"RESET" %d is sleeping\n", time_passed, philo->philo_id);
+		else if (state == DEATH)	
+			printf(RED"%-6ld"RESET" %d is dead\n", time_passed, philo->philo_id);
 	}
 	handle_mutex(&philo->data->mutex_print, UNLOCK);
 }
